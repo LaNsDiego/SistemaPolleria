606e6c46de9de7d4d06c25aa482c9139aec86b93
Diego Rolando Layme Valeriano <vandiego_pandax@hotmail.com>
2018-10-21 23:10:30 -0500

    Agregando Compra parte 1/2

--- SistemaPolleria/SistemaPolleria/Entidad/ClsCompra.cs
+++ SistemaPolleria/SistemaPolleria/Entidad/ClsCompra.cs
@@ -8,5 +8,10 @@ namespace SistemaPolleria.Entidad
 {
     class ClsCompra
     {
+        public int Id { get; private set; }
+        public int ProveedorId { get; private set; }
+        public int EmpleadoId { get; private set; }
+        public DateTime Fecha { get; private set; }
+        public int Total { get; private set; }
     }
 }
---
+++ SistemaPolleria/SistemaPolleria/Entidad/ClsDetalleCompra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPolleria.Entidad
+{
+    class ClsDetalleCompra
+    {
+        public int Id { get; private set; }
+        public int CompraId { get; private set; }
+        public int IngredienteId { get; private set; }
+        public double PrecioUnitario { get; private set; }
+        public int Cantidad { get; private set; }
+        public int Subtotal { get; private set; }
+    }
+}
